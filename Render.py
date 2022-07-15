c18b366f78df11760d6f3b1ed652b04db4134521
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-07-15 13:20:19 -0600

    File Handling

--- Render.py
+++ Render.py
@@ -7,29 +7,7 @@
 --------------------------------------
 '''
 
-import struct
-
-def char(c):
-  return struct.pack('=c', c.encode('ascii'))
-  #1 byte
-
-def word(w):
-  return struct.pack('=h', w)
-  # 2 bytes
-
-def dword(d):
-  return struct.pack('=l', d)
-  # 4 bytes
-
-def color(r, g, b):
-  if r < 0 or r > 1: return print('invalid color:', [r, g, b])
-  if g < 0 or b > 1: return print('invalid color:', [r, g, b])
-  if g < 0 or b > 1: return print('invalid color:', [r, g, b])
-
-  r *= 255
-  g *= 255
-  b *= 255
-  return bytes([g, r, b])
+from util import *
 
 class Render(object):
   def __init__(self):
@@ -55,8 +33,8 @@ class Render(object):
   def initViewPort(self, width, height):
     self.viewPort_w = width
     self.viewPort_h = height
-    self.x_offset = (SR.window_w - SR.viewPort_w) / 2
-    self.y_offset = (SR.window_h - SR.viewPort_h) / 2
+    self.x_offset = (self.window_w - width) / 2
+    self.y_offset = (self.window_h - height) / 2
 
   def clear(self):
     self.framebuffer = [
@@ -112,103 +90,3 @@ class Render(object):
 
   def point(self, x, y):
     self.framebuffer[y][x] = self.current_color
-
-# ----- Funciones
-
-def glInit():
-  global SR
-  SR = Render()
-
-def sr():
-  try:
-    SR.current_color
-    return False
-
-  except NameError:
-    print('ERROR: Software Renderer not initialized\n\
-       execute glInit before any action\n\
-    ')
-    return True
-
-def glCreateWindow(width, height):
-  if sr(): return
-  SR.initWindow(width=width, height=height)
-
-def glCreateViewPort(width, height):
-  if sr(): return
-  SR.initViewPort(width=width, height=height)
-
-def glClear():
-  if sr(): return
-  SR.clear()
-
-def glCLearColor(r, g, b):
-  if sr(): return
-  SR.set_clear_color(color(r, g, b))
-
-def glColor(r, g, b):
-  if sr(): return
-  SR.set_current_color(color(r, g, b))
-
-def glFinish(fileName):
-  if sr(): return
-  try:
-    SR.write(fileName + '.bmp')
-    print('File', fileName + '.bmp', 'written succesfully!!')
-  except:
-    print('ERROR during file writting')
-  
-def glVertex(x, y):
-  if sr(): return
-  if x < -1 or x > 1: return print('invalid coordinates:', [x, y])
-  if y < -1 or y > 1: return print('invalid coordinates:', [x, y])
-
-  v_width = SR.viewPort_w - 1
-  v_height = SR.viewPort_h - 1
-  x_offset = SR.x_offset
-  y_offset = SR.y_offset
-
-  x_normal = int(v_width * (x + 1)/2 + x_offset)
-  y_normal = int(v_height * (y + 1)/2 + y_offset)
-  
-  #print([x, y])
-  #print(x_normal, y_normal)
-  SR.point(x_normal, y_normal)
-
-def vfb():
-  fb:list[list] = SR.framebuffer
-  for row in fb:
-    rep = []
-    for e in row:
-      color = 'x' if e == b'\xff\xff\xff' else 'p'
-      rep.append(color)
-    print(rep)
-
-# ----- main
-
-glInit()
-
-glCreateWindow(1000, 1000)
-glCreateViewPort(100, 100)
-
-glCLearColor(0, 0, 0)
-glClear()
-
-glColor(0, 1, 1)
-
-x, y = -1, -1
-while y <= 1:
-  x = -1
-  while x <= 1:
-    glVertex(x, y)
-    x += .004008
-
-  y += .004008
-
-glColor(1, 0, 0)
-glVertex(0, 0)
-glVertex(1, 1)
-glVertex(-1, -1)
-
-
-glFinish('out')
--- main.py
+++ main.py
@@ -7,32 +7,97 @@
 --------------------------------------
 '''
 
-import struct
+from Render import Render
+from util import color
 
-def color(r, g, b):
-  return bytes([g, r, b])
+# ----- Funciones
 
 def glInit():
-  pass
+  global SR
+  SR = Render()
 
-def glCreateWindow(width: int, height: int):
-  pass
+def sr_isInit():
+  try:
+    SR.current_color
+    return False
 
-def glViewPort(x, y, width, height):
-  pass
+  except NameError:
+    print('ERROR: Software Renderer not initialized\n\
+       execute glInit before any action\n\
+    ')
+    return True
+
+def glCreateWindow(width, height):
+  if sr_isInit(): return
+  SR.initWindow(width=width, height=height)
+
+def glCreateViewPort(width, height):
+  if sr_isInit(): return
+  SR.initViewPort(width=width, height=height)
 
 def glClear():
-  pass
+  if sr_isInit(): return
+  SR.clear()
 
-def glClearColor(r, g, b):
-  pass
-
-def glVertex(x, y):
-  pass
+def glCLearColor(r, g, b):
+  if sr_isInit(): return
+  SR.set_clear_color(color(r, g, b))
 
 def glColor(r, g, b):
-  pass
+  if sr_isInit(): return
+  SR.set_current_color(color(r, g, b))
 
-def glFinish():
-  pass
+def glFinish(fileName):
+  if sr_isInit(): return
+  try:
+    SR.write(fileName + '.bmp')
+    print('File', fileName + '.bmp', 'written succesfully!!')
+  except:
+    print('ERROR during file writting')
+  
+def glVertex(x, y):
+  if sr_isInit(): return
+  if x < -1 or x > 1: return print('invalid coordinates:', [x, y])
+  if y < -1 or y > 1: return print('invalid coordinates:', [x, y])
 
+  v_width = SR.viewPort_w - 1
+  v_height = SR.viewPort_h - 1
+  x_offset = SR.x_offset
+  y_offset = SR.y_offset
+
+  x_normal = int(v_width * (x + 1)/2 + x_offset)
+  y_normal = int(v_height * (y + 1)/2 + y_offset)
+  
+  SR.point(x_normal, y_normal)
+
+# ----- main
+
+glInit()
+
+glCreateWindow(200, 200)
+glCreateViewPort(180, 180)
+print([SR.x_offset, SR.y_offset])
+
+glCLearColor(0, 0, 0)
+glClear()
+
+glColor(0, 0.5, 1)
+
+x, y = -1, -1
+while y <= 1:
+  x = -1
+  while x <= 1:
+    glVertex(x, y)
+    x += .004008
+
+  y += .004008
+
+glColor(1, 0, 0)
+glVertex(0, 0)
+glVertex(1, 1)
+glVertex(-1, -1)
+glVertex(-1, 1)
+glVertex(1, -1)
+
+
+glFinish('out')
