62a7789744452feabe3c52644242ff4da6353b5c
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-08-17 12:17:14 -0600

    FInal Rendering

--- Drivers.py
+++ Drivers.py
@@ -1,4 +1,26 @@
+''' 
+--------------------------------------
+  Universidad del Valle de Guatemala
+  Author: Diego Cordova - 20212
+
+  Drivers.py
+  - Have the instructions to render 
+    models using gl.py functions
+  
+  Last modified (yy-mm-dd): 2022-08-17 
+--------------------------------------
+'''
+
 from src.gl import *
+
+def __Init(size):
+  # Initalization
+  glInit()
+  x, y = size if type(size) in [tuple, list] else size, size
+
+  # Viewport and window initialization
+  glCreateWindow  (x, y)
+  glCreateViewPort(x, y)
 
 def __paintModel(model, texture, W, transform, scale, v_to_draw, L):
   print('\nRenderizando:', model, '\nCon texturas:', texture)
@@ -10,51 +32,34 @@ def __paintModel(model, texture, W, transform, scale, v_to_draw, L):
   )
 
   # File Writing
-  glFinish('./renders/Mario')
+  glFinish('./renders/' + model.split('/')[-1].split('.')[0])
 
 
-def __Init(size):
-  # Initalization
-  glInit()
-  x, y = size if type(size) in [tuple, list] else size, size
-
-  # Viewport and window initialization
-  glCreateWindow  (x, y)
-  glCreateViewPort(x, y)
+# ------ Modelos ------
 
 def Mario():
   model = './models/Mario/Mario.obj'
   texture = './models/Mario/Mario.bmp'
-  print('\nRenderizando:', model, '\nCon texturas:', texture)
-
+  v_to_draw = (2, 0, 1) # z, x, y
+  LIGHT = (-15, 10, -100)
   W = 1024
-  __Init(W)
-  # --- Mario ---
 
-  # Obj Model Drawing
   transform = (
     round(W / 2),
     round(W / 2),
     round(W * 2)
   )
-
   scale = (
     round(W / 2.8),
     round(W / 2.8), 
     round(W / 4)
   )
 
-  v_to_draw = (2, 0, 1) # z, x, y
-  LIGHT = (15, 30, -100)
-  LIGHT = (-15, 10, -100)
-
-  load_model(
-    model, transform, scale, L=LIGHT, 
-    vertex_to_draw=v_to_draw , texture_path=texture
+  __paintModel(
+    model, texture, W, transform, 
+    scale, v_to_draw, LIGHT
   )
-
-  # File Writing
-  glFinish('./renders/Mario')
+  
 
 def Tmap_Mario():
   glInit()
@@ -69,47 +74,29 @@ def Tmap_Mario():
 def Yoshi():
   model = './models/Yoshi/Yoshi.obj'
   texture = './models/Yoshi/Yoshi.bmp'
-  print('\nRenderizando:', model, '\nCon texturas:', texture)
-  
-  # Initalization
-  glInit()
-
-  # Viewport and window initialization
+  v_to_draw = (2, 0, 1) # z, x, y
+  LIGHT = (0, 0, -100)
   W = 1024
-  glCreateWindow  (W, W)
-  glCreateViewPort(W, W)
 
-  # --- Mario ---
-
-  # Obj Model Drawing
   transform = (
     round(W),
     round(W / 2),
     round(W * 2)
   )
-
   scale = (
     round(W / 2.8),
     round(W / 2.8), 
     round(W / 4)
   )
 
-
-  v_to_draw = (2, 0, 1) # z, x, y
-  LIGHT = (0, 0, -100)
-
-  load_model(
-    model, transform, scale, L=LIGHT, 
-    vertex_to_draw=v_to_draw , texture_path=texture
+  __paintModel(
+    model, texture, W, transform, 
+    scale, v_to_draw, LIGHT
   )
-
-  # File Writing
-  glFinish('./renders/Yoshi')
 
 
 def Tmap_Yoshi():
   glInit()
-
   model = './models/Yoshi/Yoshi.obj'
   texture = './models/Yoshi/Yoshi.bmp'
 
@@ -121,46 +108,28 @@ def Tmap_Yoshi():
 def Face():
   model = './models/Face/face.obj'
   texture = './models/Face/model.bmp'
-  print('\nRenderizando:', model, '\nCon texturas:', texture)
-  
-  # Initalization
-  glInit()
-
-  # Viewport and window initialization
+  v_to_draw = (0, 1, 2) # z, x, y
+  LIGHT = (0, 0, -100)
   W = 1024
-  glCreateWindow  (W, W)
-  glCreateViewPort(W, W)
 
-  # --- Mario ---
-
-  # Obj Model Drawing
   transform = (
     round(W / 2),
     round(W / 2),
     round(W * 2)
   )
-
   scale = (
     round(W / 2.1),
     round(W / 2.1), 
     round(W / 4)
   )
 
-
-  v_to_draw = (0, 1, 2) # z, x, y
-  LIGHT = (0, 0, -100)
-
-  load_model(
-    model, transform, scale, L=LIGHT, 
-    vertex_to_draw=v_to_draw , texture_path=texture
+  __paintModel(
+    model, texture, W, transform, 
+    scale, v_to_draw, LIGHT
   )
-
-  # File Writing
-  glFinish('./renders/Face')
 
 def Tmap_Face():
   glInit()
-
   model = './models/Face/face.obj'
   texture = './models/Face/model.bmp'
   render_img(texture, model, './renders/Face_texture')
